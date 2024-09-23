d335fca5837273ae359ed560713f3522bf709159
Vihaan Bhaduri <vihaan.bhaduri@gmail.com>
2024-09-23 09:18:20 +0000

    Added 30th run for in_place_flight for Crazyflie 2.1+. Also added code such that grapher.py draws lines between each value of the scatter plot for the logger data to its respective point for the manual data.

--- examples/log_data/grapher.py
+++ examples/log_data/grapher.py
@@ -27,10 +27,10 @@ for i in range(len(df1)):
         if i == 25 or i == 35:
             continue
 
-    x1.append(39.3701*df1["X-Net"][i])
-    y1.append(39.3701*df1["Y-Net"][i])
-    x2.append(df2["X-Net"][i])
-    y2.append(df2["Y-Net"][i])
+    x1.append(100*df1["X-Net"][i])
+    y1.append(100*df1["Y-Net"][i])
+    x2.append(2.54*df2["X-Net"][i])
+    y2.append(2.54*df2["Y-Net"][i])
 
 for i in range(len(x1)):
     x_sum_logger += x1[i]
@@ -49,12 +49,17 @@ plt.scatter(x_sum_logger/len(x1), y_sum_logger/len(x1), label="Average Point for
 plt.scatter(x_sum_manual/len(x2), y_sum_manual/len(x2), label="Average Point for Manual Data", marker='X', s=90)
 plt.scatter(0, 0, color='0', marker='X', s=100)
 
+for i in range(len(x1)):
+    x = [x1[i], x2[i]]
+    y = [y1[i], y2[i]]
+    plt.plot(x, y, color="black", linewidth=1, alpha=0.225)
+
 plt.legend(loc = "upper left")#bbox_to_anchor=(1.05, 1), loc = "upper left")
 
 plt.title("Net Drone Drift for In Place Flight")
 
-plt.xlabel("Horizontal (X) Drift (Inches)")
-plt.ylabel("Vertical (Y) Drift (Inches)")
+plt.xlabel("Horizontal (X) Drift (Centimeters)")
+plt.ylabel("Vertical (Y) Drift (Centimeters)")
 
 plt.grid()
 
